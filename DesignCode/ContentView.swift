bc0b30cc529deb3faf200bda80eb85c083c46d46
ehsan2793 <ehsan2793@gmail.com>
2023-07-15 12:42:30 -0700

    added first card

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("hello")
+        ZStack {
+            VStack {
+                Spacer()
+                    .frame(width: 300, height: 220)
+                    .background(.blue)
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                    .offset(x: 0, y: -20)
+            } //: VSTACK
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    } //: VSTACK
+                    Spacer()
+                    Image("Logo1")
+                } //: HSTACK
+                .padding()
+                Spacer()
+                Image("Card1")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 300, height: 110, alignment: .top)
+            } //: VSTACK
+            .frame(width: 340, height: 220)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+        } //: ZSTACK
     }
 }
 
