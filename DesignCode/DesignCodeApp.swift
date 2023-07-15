e5092b0e8de760c84bcfaf32eb937d3846a742a9
ehsan2793 <ehsan2793@gmail.com>
2023-07-15 11:04:33 -0700

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Ehsan Rahimi on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
