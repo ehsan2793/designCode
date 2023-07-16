2727c0d06b8307806a4eed1afe2fe5c9143b9728
ehsan2793 <ehsan2793@gmail.com>
2023-07-15 22:19:07 -0700

    added simple animation

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,37 +10,47 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTIES
 
+    @State private var show = false
+
     // MARK: - BODY
 
     var body: some View {
         ZStack {
             TitleView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default, value: show)
 
             BackCardView() //: VSTACK
-                .background(Color("card4"))
+                .background(show ? Color("card3") : Color("card4"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -40)
+                .offset(x: 0, y: show ? -400 : -40)
                 .scaleEffect(0.9)
-                .rotationEffect(Angle(degrees: 10))
+                .rotationEffect(Angle(degrees: show ? 0 : 10))
                 .rotation3DEffect(Angle(degrees: 10), axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5), value: show)
 
             BackCardView() //: VSTACK
-                .background(Color("card3"))
+                .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -20)
+                .offset(x: 0, y: show ? -210 : -20)
                 .scaleEffect(0.95)
-                .rotationEffect(Angle(degrees: 5))
+                .rotationEffect(Angle(degrees: show ? 0 : 5))
                 .rotation3DEffect(Angle(degrees: 5), axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3), value: show)
 
             CardView()
+                .blendMode(.hardLight)
+                .onTapGesture {
+                    show.toggle()
+                }
 
             BottomCardView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default, value: show)
         } //: ZSTACK
     }
 }
