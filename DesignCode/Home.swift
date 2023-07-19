13be48751aa4ca20a50a6bafa7550843a6aff9ce
ehsan2793 <ehsan2793@gmail.com>
2023-07-18 19:11:13 -0700

    homeView

---
+++ DesignCode/Home.swift
@@ -0,0 +1,78 @@
+//
+//  HomeView.swift
+//  DesignCode
+//
+//  Created by Ehsan Rahimi on 7/17/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    // MARK: - PROPERTIES
+
+    @State private var showProfile = false
+    @State var viewState = CGSize.zero
+
+    // MARK: - BODY
+
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.5)
+                .edgesIgnoringSafeArea(.all)
+            HomeView(showProfile: $showProfile)
+                .padding(.top, 44)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+                .offset(y: showProfile ? -450 : 0)
+                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
+                .scaleEffect(showProfile ? 0.9 : 1)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: showProfile)
+                .edgesIgnoringSafeArea(.all)
+
+            MenuView()
+                .background(Color.black.opacity(0.0001))
+                .offset(y: showProfile ? 0 : 1000)
+                .offset(y: viewState.height)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: showProfile)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: viewState.height < 0)
+                .onTapGesture {
+                    showProfile.toggle()
+                }
+
+                .gesture(
+                    DragGesture().onChanged { value in
+                        if value.translation.height > -310 {
+                            viewState = value.translation
+                        }
+                    }.onEnded { _ in
+                        if viewState.height > 50 {
+                            showProfile = false
+                        }
+                        viewState = .zero
+                    }
+                )
+        } //: ZSTACK
+    }
+}
+
+// MARK: PREVIEW
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+struct AvatarView: View {
+    @Binding var showProfile: Bool
+    var body: some View {
+        Button(action: { showProfile.toggle() }) {
+            Image("Avatar")
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+        }
+    }
+}
--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -2,70 +2,31 @@
 //  HomeView.swift
 //  DesignCode
 //
-//  Created by Ehsan Rahimi on 7/17/23.
+//  Created by Ehsan Rahimi on 7/18/23.
 //
 
 import SwiftUI
 
 struct HomeView: View {
     // MARK: - PROPERTIES
-
-    @State private var showProfile = false
-    @State var viewState = CGSize.zero
-
+    @Binding var showProfile: Bool
     // MARK: - BODY
 
     var body: some View {
-        ZStack {
-            Color.gray.opacity(0.5)
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                HStack {
-                    Text("Watching")
-                        .font(.system(size: 28, weight: .bold))
-
-                    Spacer()
-
-                    AvatarView(showProfile: $showProfile)
-                } //: HSTACK
-                .padding(.horizontal)
-                .padding(.top, 30)
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
 
                 Spacer()
-            } //: VSTACK
-            .padding(.top, 44)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30))
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
-            .scaleEffect(showProfile ? 0.9 : 1)
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: showProfile)
-            .edgesIgnoringSafeArea(.all)
-           
-            MenuView()
-                .background(Color.black.opacity(0.0001))
-                .offset(y: showProfile ? 0 : 1000)
-                .offset(y: viewState.height)
-                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: showProfile)
-                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: viewState.height < 0)
-                .onTapGesture {
-                    showProfile.toggle()
-                }
-            
-                .gesture(
-                    DragGesture().onChanged { value in
-                        if value.translation.height > -310 {
-                            viewState = value.translation
-                        }
-                    }.onEnded { _ in
-                        if viewState.height > 50 {
-                            showProfile = false
-                        }
-                        viewState = .zero
-                    }
-                )
-        } //: ZSTACK
+
+                AvatarView(showProfile: $showProfile)
+            } //: HSTACK
+            .padding(.horizontal)
+            .padding(.top, 30)
+
+            Spacer()
+        } //: VSTACK
     }
 }
 
@@ -73,19 +34,6 @@ struct HomeView: View {
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
-    }
-}
-
-struct AvatarView: View {
-    @Binding var showProfile: Bool
-    var body: some View {
-        Button(action: { showProfile.toggle() }) {
-            Image("Avatar")
-                .renderingMode(.original)
-                .resizable()
-                .frame(width: 36, height: 36)
-                .clipShape(Circle())
-        }
+        HomeView(showProfile: .constant(true) )
     }
 }
