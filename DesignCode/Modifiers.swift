748a5eaec9294c7e4a08d0052d9be6d3eb2ea6c1
ehsan2793 <ehsan2793@gmail.com>
2023-07-24 20:17:51 -0700

    create new view for ring

--- DesignCode/Modifiers.swift
+++ DesignCode/Modifiers.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ShadowModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 10)
+            .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
             .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
     }
 }
