7113ed1a96952d28300bc20b8543f587d81c965c
ehsan2793 <ehsan2793@gmail.com>
2023-07-25 20:24:34 -0700

    card added

--- DesignCode/CourseList.swift
+++ DesignCode/CourseList.swift
@@ -14,16 +14,8 @@ struct CourseList: View {
 
     var body: some View {
         VStack {
-            HStack(alignment: .top) {
-                VStack(alignment: .leading, spacing: 8.0) {
-                    Text("SwiftUi Advance")
-                        .font(.system(size: 24, weight: .bold))
-                    Text("20 Sections")
-                } //: VSTACK
-                Image("Logo1")
-            } //: HSTACK
-            Image("Card2")
-        } //: VSTCK
+            CourseView()
+        }
     }
 }
 
@@ -32,5 +24,40 @@ struct CourseList: View {
 struct CourseList_Previews: PreviewProvider {
     static var previews: some View {
         CourseList()
+    }
+}
+
+struct CourseView: View {
+    var backgroundColor = Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))
+    var shadowColor = Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))
+
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading, spacing: 8.0) {
+                    Text("SwiftUi Advance")
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.white)
+
+                    Text("20 Sections")
+                        .foregroundColor(Color.white.opacity(0.7))
+                } //: VSTACK
+                Spacer()
+
+                Image("Logo1")
+            } //: HSTACK
+            Spacer()
+
+            Image("Card2")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .frame(height: 140, alignment: .top)
+        } //: VSTCK
+        .padding(20)
+        .frame(width: screen.width - 60, height: 280)
+        .background(backgroundColor)
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: shadowColor.opacity(0.3), radius: 20, x: 0, y: 20)
     }
 }
