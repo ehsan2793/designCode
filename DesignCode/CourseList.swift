ab58152a436145d0fe64f8764f120c067ea7ff9e
ehsan2793 <ehsan2793@gmail.com>
2023-07-25 21:09:16 -0700

    made card component

--- DesignCode/CourseList.swift
+++ DesignCode/CourseList.swift
@@ -28,8 +28,13 @@ struct CourseList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
+    // MARK: - PROPERTIES
+
+    @State var show = false
     var backgroundColor = Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))
     var shadowColor = Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))
+
+    // MARK: - BODY
 
     var body: some View {
         VStack {
@@ -54,10 +59,17 @@ struct CourseView: View {
                 .frame(maxWidth: .infinity)
                 .frame(height: 140, alignment: .top)
         } //: VSTCK
-        .padding(20)
-        .frame(width: screen.width - 60, height: 280)
+        .padding(show ? 30 : 20)
+        .padding(.top, show ? 30 : 0)
+//        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
+        .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280)
         .background(backgroundColor)
         .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
         .shadow(color: shadowColor.opacity(0.3), radius: 20, x: 0, y: 20)
+        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: show)
+        .onTapGesture {
+            show.toggle()
+        }
+        .ignoresSafeArea(.all)
     }
 }
