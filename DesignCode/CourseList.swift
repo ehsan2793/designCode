e2699c2d25e17696a11e0ddb1dd6a5d59df05da5
ehsan2793 <ehsan2793@gmail.com>
2023-07-26 22:16:15 -0700

    fix spacing

--- DesignCode/CourseList.swift
+++ DesignCode/CourseList.swift
@@ -43,6 +43,7 @@ struct CourseView: View {
                 Text("About this course")
                     .font(.title)
                     .bold()
+
                 Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
 
                 Text("Minimal coding experience required, such as in HTML and CSS. Please note that Xcode 11 and Catalina are essential. Once you get everything installed, it'll get a lot friendlier! I added a bunch of troubleshoots at the end of this page to help you navigate the issues you might encounter.")
